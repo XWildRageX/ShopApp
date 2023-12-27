7fbbc43b6c5d66f3ac8e8d3dfe085a03ac8b14d9
StarConstellation <danya.akimov.03@gmail.com>
2023-12-28 00:46:40 +0300

    f

---
+++ ShopApp/Connection/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Connection
+{
+    public class DB
+    {
+        public static ShopEntities entities = new ShopEntities();
+    }
+}
